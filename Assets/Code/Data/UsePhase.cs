a0c1d87b06a0666a873a95f47a9cb7f07ece7919
Furkan Kambay <furkankambay@protonmail.com>
2025-03-28 02:33:04 +0300

    fix(tool): invisible UsePhase inline inspectors

--- Assets/Code/Data/UsePhase.cs
+++ Assets/Code/Data/UsePhase.cs
@@ -8,21 +8,21 @@ namespace Tulip.Data
     [Serializable]
     public struct UsePhase
     {
-        [LayoutGroup("", ELayout.Background)]
+        [LayoutGroup(".", ELayout.Background)]
 
-        [LayoutGroup("/Options", ELayout.TitleOut)]
+        [LayoutGroup("./Options", ELayout.TitleOut)]
         public bool isCancelable;
         public bool preventAim;
         public bool shouldHit;
 
-        [LayoutGroup("/Transform", ELayout.TitleOut | ELayout.Horizontal)]
+        [LayoutGroup("./Transform", ELayout.TitleOut | ELayout.Horizontal)]
         [PostFieldRichLabel("in")]
         public Vector2 moveDelta;
 
         [OverlayRichLabel("<color=grey>sec")]
         public float moveDuration;
 
-        [LayoutGroup("/Turn", ELayout.Horizontal)]
+        [LayoutGroup("./Turn", ELayout.Horizontal)]
         [PostFieldRichLabel("in")]
         [OverlayRichLabel("<color=grey>deg")]
         public float turnDelta;
