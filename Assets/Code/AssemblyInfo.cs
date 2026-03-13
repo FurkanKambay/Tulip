a7e6e088ceee7d2978462f144b102d577c2dc650
Furkan Kambay <furkankambay@protonmail.com>
2026-03-13 19:08:17 +0300

    chore(project): add AssemblyInfo with IsExternalInit, expose internals to Editor assembly

---
+++ Assets/Code/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+
+[assembly: InternalsVisibleTo("Tulip.Editor")]
+
+// ReSharper disable CheckNamespace
+namespace System.Runtime.CompilerServices
+{
+    [UsedImplicitly]
+    public static class IsExternalInit
+    {
+    }
+}
