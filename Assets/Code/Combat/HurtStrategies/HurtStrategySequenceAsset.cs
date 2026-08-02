44bd7daec44040861f923fe150069bd0f356f966
Furkan Kambay <furkankambay@protonmail.com>
2026-08-02 15:30:41 +0300

    feat: implement Hurt Strategy Sequence

---
+++ Assets/Code/Combat/HurtStrategies/HurtStrategySequenceAsset.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace FK.Tulip.Combat
+{
+    [CreateAssetMenu(menuName = "Strategies/Get Hurt/Strategy Sequence", order = 100)]
+    public class HurtStrategySequenceAsset : BaseHurtStrategyAsset, IHurtStrategy
+    {
+        [SerializeField] private BaseHurtStrategyAsset[] strategies;
+
+        public override bool Apply(Health victim, IWeapon weapon, DamageType? damageTypeOverride = null)
+        {
+            bool allSucceeded = true;
+
+            foreach (BaseHurtStrategyAsset strategy in strategies)
+            {
+                if (!strategy.Apply(victim, weapon, damageTypeOverride))
+                    allSucceeded = false;
+            }
+
+            return allSucceeded;
+        }
+    }
+}
